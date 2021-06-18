a9a9471bdf5d56693651a48229f2ca8380c7220d
hermtechs <38890522+hermtechs@users.noreply.github.com>
2021-06-18 09:08:01 +0300

    deleted updateaTotal() function and trying to reimplement it due to a logical bug previously influenced

--- shop.js
+++ shop.js
@@ -1,42 +1,42 @@
 // NAVIGATION MENU
-const humburger = document.getElementById('nav-toggle-checkbox');
-const navSpan = document.querySelector('.nav-span');
-const quickViewBtn = document.querySelectorAll('.view-product-btn');
-const quickViewOverlay = document.querySelector('.quick-view-overlay')
-const quickViewContainer = document.querySelector('.quick-view-container')
-const quickViewWindow = document.querySelector('.quick-view-container')
-const closeCartBtn = document.querySelector('.close-cart')
-const cartContainer = document.querySelector('.cart');
-const cartItemsContainer = document.querySelector('.cart-items')
-const cartOverlay = document.querySelector('.cart-overlay')
-const cartIcon = document.querySelectorAll('.cart-btn');
-const addToCartBtn = document.querySelectorAll('.add-to-cartBtn');
+const humburger = document.getElementById("nav-toggle-checkbox");
+const navSpan = document.querySelector(".nav-span");
+const quickViewBtn = document.querySelectorAll(".view-product-btn");
+const quickViewOverlay = document.querySelector(".quick-view-overlay");
+const quickViewContainer = document.querySelector(".quick-view-container");
+const quickViewWindow = document.querySelector(".quick-view-container");
+const closeCartBtn = document.querySelector(".close-cart");
+const cartContainer = document.querySelector(".cart");
+const cartItemsContainer = document.querySelector(".cart-items");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartIcon = document.querySelectorAll(".cart-btn");
+const addToCartBtn = document.querySelectorAll(".add-to-cartBtn");
 
-
-humburger.addEventListener('click', ()=>{
-    if(navSpan.style.transform === 'rotate(8deg)'){
-     navSpan.style.transform = 'rotate(0deg)'
-    }
-    else{
-     navSpan.style.transform = 'rotate(8deg)'
-    }
-    })
+humburger.addEventListener("click", () => {
+  if (navSpan.style.transform === "rotate(8deg)") {
+    navSpan.style.transform = "rotate(0deg)";
+  } else {
+    navSpan.style.transform = "rotate(8deg)";
+  }
+});
 
 //SHOP
-quickViewBtn.forEach(btn=>{
-    btn.addEventListener('click', createQuickViewWindow);
-})
-    
-function createQuickViewWindow(event){
-// const quickViewWindow = document.createElement('article');    
-const clickedSpecificBtn = event.target
-clickedBtnParent = clickedSpecificBtn.parentElement.parentElement.parentElement;
-productImgUrl = clickedBtnParent.querySelector('.product-image').src;
-productTitle = clickedBtnParent.querySelector('.product-name').innerText;
-productPrice = clickedBtnParent.querySelector('.prod-price').innerText;
-// console.log(productImgUrl,productPrice,productTitle);
+quickViewBtn.forEach((btn) => {
+  btn.addEventListener("click", createQuickViewWindow);
+});
 
-quickViewWindow.innerHTML = `
+function createQuickViewWindow(event) {
+  // const quickViewWindow = document.createElement('article');
+  const clickedSpecificBtn = event.target;
+  const clickedBtnParent =
+    clickedSpecificBtn.parentElement.parentElement.parentElement;
+  const productImgUrl = clickedBtnParent.querySelector(".product-image").src;
+  const productTitle = clickedBtnParent.querySelector(".product-name")
+    .innerText;
+  const productPrice = clickedBtnParent.querySelector(".prod-price").innerText;
+  // console.log(productImgUrl,productPrice,productTitle);
+
+  quickViewWindow.innerHTML = `
 <i class="fas fa-times closeQuick-view"></i>
 <div class="quick-viewImg-container">
 <img src="${productImgUrl}" alt="" class="quick-img">
@@ -58,101 +58,94 @@ quickViewWindow.innerHTML = `
 </div>
 </div>
 </div>
-`    
-quickViewOverlay.appendChild(quickViewWindow);
-quickViewOverlay.style.display= 'flex';    
+`;
+  quickViewOverlay.appendChild(quickViewWindow);
+  quickViewOverlay.style.display = "flex";
 
-const closeQuickViewBtn = quickViewWindow.querySelector('.closeQuick-view')
-closeQuickViewBtn.addEventListener('click',()=>{
-    quickViewOverlay.style.display= 'none';
-    })
-  
+  const closeQuickViewBtn = quickViewWindow.querySelector(".closeQuick-view");
+  closeQuickViewBtn.addEventListener("click", () => {
+    quickViewOverlay.style.display = "none";
+  });
+
   //closing quickView window on click
-//   quickViewOverlay.addEventListener('click',()=>{
-//       quickViewOverlay.style.display='none';
-//   })
+  //   quickViewOverlay.addEventListener('click',()=>{
+  //       quickViewOverlay.style.display='none';
+  //   })
 }
 
- //closing overlays ie cart and quickView on pressing Esc key
- document.addEventListener('keydown',(event)=>{
-    if(event.code=='Escape'){
-    quickViewOverlay.style.display = 'none';
-    cartOverlay.style.visibility='hidden'
-    cartContainer.style.transform = 'translateX(110%)'
-    }
- }) 
+//closing overlays ie cart and quickView on pressing Esc key
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Escape") {
+    quickViewOverlay.style.display = "none";
+    cartOverlay.style.visibility = "hidden";
+    cartContainer.style.transform = "translateX(110%)";
+  }
+});
 
 // CART
 //opening cart
-cartContainer.style.transform='translateX(110%)'
-cartIcon.forEach(icon=>{ icon.addEventListener('click', ()=>{
-    if(cartContainer.style.transform==='translateX(110%)'){
-     cartContainer.style.transform = 'translateX(0%)'
-     cartOverlay.style.visibility = 'visible'
+cartContainer.style.transform = "translateX(110%)";
+cartIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    if (cartContainer.style.transform === "translateX(110%)") {
+      cartContainer.style.transform = "translateX(0%)";
+      cartOverlay.style.visibility = "visible";
+    } else {
+      cartContainer.style.transform = "translateX(110%)";
+      cartOverlay.style.visibility = "hidden";
     }
-    else{
-        cartContainer.style.transform='translateX(110%)'
-        cartOverlay.style.visibility = 'hidden'
-    }
-})
-}
-)
+  });
+});
 
-closeCartBtn.addEventListener('click',()=>{
-     cartContainer.style.transform='translateX(110%)'
-     cartOverlay.style.visibility = 'hidden'
-})
+closeCartBtn.addEventListener("click", () => {
+  cartContainer.style.transform = "translateX(110%)";
+  cartOverlay.style.visibility = "hidden";
+});
 //CART AND CALCULATIONS
 
 // const itemPrice = document.querySelector('.price');
 
-function removeCartItem(event){
-    const specificClickedBtn = event.target;
-    specificClickedBtn.parentElement.parentElement.remove();
-    updateTotal();
-    }
+function removeCartItem(event) {
+  const specificClickedBtn = event.target;
+  specificClickedBtn.parentElement.parentElement.remove();
+  updateTotal();
+}
 
-function updateTotal(){
- var cartRow = cartContainer.querySelectorAll('.cart-item');
- var total = 0;
- for(var i=0; i<cartRow.length; i++){
+/*function updateTotal() {
+  var cartRow = cartContainer.querySelectorAll(".cart-item");
+  var total = 0;
+  for (var i = 0; i < cartRow.length; i++) {
     const eachRow = cartRow[i];
-    const eachItemPrice = eachRow.querySelectorAll('.price')[0].innerText
-    const eachItemPriceValue=  eachItemPrice.replace('$','');
-    
-    const eachItemQuantity= eachRow.querySelectorAll('.item-quantity')[0].innerText
-    total = ( total + parseFloat(eachItemPriceValue * eachItemQuantity) );
- };  
- const totalElement = cartContainer.querySelector('.total');
- totalElement.innerText = total
-}
+    const eachItemPrice = eachRow.querySelectorAll(".price")[0].innerText;
+    const eachItemPriceValue = eachItemPrice.replace("$", "");
+
+    const eachItemQuantity = eachRow.querySelectorAll(".item-quantity")[0]
+      .innerText;
+    total = total + parseFloat(eachItemPriceValue * eachItemQuantity);
+  }
+  const totalElement = cartContainer.querySelector(".total");
+  totalElement.innerText = total;
+}*/
 //Adding a product to a cart
-addToCartBtn.forEach(btn=>{
-    btn.addEventListener('click',addItemToCart)
-})
-function addItemToCart(event){
- const theClickedBtn = event.target;
- const productContainer = theClickedBtn.parentElement.parentElement;
- var prodPrice = productContainer.querySelector('.prod-price').innerText;
- var prodImageUrl = productContainer.querySelector('.product-image').src;
- var productName = productContainer.querySelector('.product-name').innerText;
- addProductToCart(productName, prodImageUrl, prodPrice)
-// fndTotal();
-openAndCloseCart();
-// checkItemAlreadyAdded(productName)
+addToCartBtn.forEach((btn) => {
+  btn.addEventListener("click", addItemToCart);
+});
+function addItemToCart(event) {
+  const theClickedBtn = event.target;
+  const productContainer = theClickedBtn.parentElement.parentElement;
+  var prodPrice = productContainer.querySelector(".prod-price").innerText;
+  var prodImageUrl = productContainer.querySelector(".product-image").src;
+  var productName = productContainer.querySelector(".product-name").innerText;
+  addProductToCart(productName, prodImageUrl, prodPrice);
+  // fndTotal();
+  updateTotal();
+  openAndCloseCart();
+  // checkItemAlreadyAdded(productName)
 }
 
-function addProductToCart(productName, prodImageUrl, prodPrice){
-    var  cartItemRow = document.createElement('article');
-    const productTitle = document.querySelectorAll('.product-name');
-    for(var i=0; i<productTitle.length; i++){
-        var eachProductTitle = productTitle[i];
-       if(eachProductTitle.innerText=productName){
-           alert('item already added to cart')
-       }
-    }
-
-    cartRowContents = `<article class="cart-item">
+function addProductToCart(productName, prodImageUrl, prodPrice) {
+  var cartItemRow = document.createElement("article");
+  const cartRowContents = `<article class="cart-item">
     <img src="${prodImageUrl}" alt="product" class="prod-image"> 
    <div>
       <h4>${productName}</h4>
@@ -163,29 +156,34 @@ function addProductToCart(productName, prodImageUrl, prodPrice){
       <i class="fas fa-chevron-up"></i>
       <p class="item-quantity">1</p>
       <i class="fas fa-chevron-down"></i>
-   </div>` ;
-   cartItemRow.innerHTML = cartRowContents
-   cartItemsContainer.append(cartItemRow); 
-   const removeBtn = cartContainer.querySelectorAll('.remove-item')
-   removeBtn.forEach(btn=>{
-       btn.addEventListener('click', removeCartItem)
-   })
+   </div>`;
+  cartItemRow.innerHTML = cartRowContents;
+  cartItemsContainer.append(cartItemRow);
+  const removeBtn = cartContainer.querySelectorAll(".remove-item");
+  removeBtn.forEach((btn) => {
+    btn.addEventListener("click", removeCartItem);
+  });
 }
-function openAndCloseCart(){
-    cartContainer.style.transform='translateX(0%)';
-    cartOverlay.style.visibility = 'visible';
+function openAndCloseCart() {
+  cartContainer.style.transform = "translateX(0%)";
+  cartOverlay.style.visibility = "visible";
 
-    setTimeout(()=>{
-        cartContainer.style.transform='translateX(110%)'
-        cartOverlay.style.visibility = 'hidden'  
-    },700)
+  setTimeout(() => {
+    cartContainer.style.transform = "translateX(110%)";
+    cartOverlay.style.visibility = "hidden";
+  }, 700);
 }
-// function checkItemAlreadyAdded(productName){
-//     const cartProductRow = document.querySelectorAll('.cart-item')
-//     cartProductRow.forEach(row=>{
-//     //  console.log(productName)
-//      if(row.innerText==productName){
-//          alert('item already added')
-//      }
-//     })
-// }    
+function updateTotal() {
+  const cartRows = cartContainer.querySelectorAll(".cart-item");
+  // getting All cart Row
+  console.log(cartRows);
+  for (var i = 0; i < cartRows.length; i++) {
+    const eachRow = cartRows[i];
+    const eachPrice = eachRow.querySelector(".item-price").innerText;
+    //console.log(eachPrice);
+    eachPrice.replace("$", "");
+    const eachProductQuantity = eachRow.querySelector(".item-quantity")
+      .innerText;
+    console.log(eachPrice);
+  }
+}
